f28bb96dee5840b9658a975ac571fe5a58ea928b
Alexandra <67715045+alexandrakamyabi@users.noreply.github.com>
2025-05-08 15:29:03 -0700

    updates

--- DataStructuresC++/DataStructuresC++.cpp
+++ DataStructuresC++/DataStructuresC++.cpp
@@ -3,12 +3,13 @@
 #include <unordered_map>
 #include <Bits.h>
 #include <Vector>
-#include<list>
+#include <list>
 #include <queue>
 #include <cstdlib>
 #include <chrono>
 #include <Algorithm>
 #include <iostream>
+#include <unordered_set>
 
 using std::unordered_map;
 using std::vector;
@@ -45,6 +46,37 @@ public:
         return value * other.value;
     }
 };
+
+// Function to check if subarray with zero-sum is present in a given array or not
+bool hasZeroSumSubarray(int nums[], int n) {
+    // create an empty set to store the sum of elements of each
+    // subarray `nums[0…i]`, where `0 <= i < n`
+    unordered_set<int> set;
+
+    // insert 0 into the set to handle the case when subarray with
+    // zero-sum starts from index 0
+    set.insert(0);
+
+    int sum = 0;
+
+    // traverse the given array
+    for (int i = 0; i < n; i++) {
+        // sum of elements so far
+        sum += nums[i];
+
+        // if the sum is seen before, we have found a subarray with zero-sum
+        if (set.find(sum) != set.end()) {
+            return true;
+        }
+        else {
+            // insert sum so far into the set
+            set.insert(sum);
+        }
+    }
+
+    // we reach here when no subarray with zero-sum exists
+    return false;
+}
 
 string caesarCipher(string s, int k) {
 
